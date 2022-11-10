552c9fc7a16830184954b57f5e065cc0ffae9f6b
Hugo Truchon <hugo.truchonti@gmail.com>
2022-11-10 11:30:37 -0500

    yarn2 config + light reminder for coffee startup

--- api/routes/alarm.js
+++ api/routes/alarm.js
@@ -2,8 +2,16 @@ const alarmCtrl = require('../controller/alarm')
 const BaseRoute = require('./baseRoute')
 const logger = require('../utils/simpleLogger')
 const alarmUtil = require('../utils/alarmUtil')
+const mqttApi = require('../connectors/mqttApi')
+
+const COORDINATES = {
+    lon: 45.388534,
+    lat: -71.919342
+}
 
 class alarmRoute extends BaseRoute {
+    static timeoutMap = new Map()
+
     static getRouter() {
         super.getRouter()
 
@@ -39,10 +47,40 @@ class alarmRoute extends BaseRoute {
     }
 
     static async _setCoffeeState(req, res) {
+        this._clearCoffeeAlarmReminders()
+
         if (req.body?.power === true) {
             await alarmCtrl.initCoffee()
+            // setup visual alarms
+            this.timeoutMap.set('25min', setTimeout(() => this._lightNotification(), 1000*60*25))
+            this.timeoutMap.set('45min', setTimeout(() => this._lightNotification(2), 1000*60*45))
+            this.timeoutMap.set('50min', setTimeout(() => this._lightNotification(3), 1000*60*50))
+            this.timeoutMap.set('shutdown', setTimeout(() => alarmCtrl.stopCoffee(), 1000*60*60))
         } else{
             await alarmCtrl.stopCoffee()
+        }
+    }
+
+    static async _lightNotification(count = 1) {
+        for (let i = 0; i < count; i++) {
+            await mqttApi.setColor('kitchen', {
+                color: { kelvin: 6500, brightness: 1 },
+                power: 'ON',
+                duration: 200
+            })
+            await new Promise(resolve => setTimeout(resolve, 200))
+            await mqttApi.setColor('kitchen', {
+                color: { kelvin: 6500, brightness: 0 },
+                power: 'OFF',
+                duration: 150
+            })
+            await new Promise(resolve => setTimeout(resolve, 1000))
+        }
+    }
+
+    static _clearCoffeeAlarmReminders() {
+        for (const timeout of this.timeoutMap.values()) {
+            clearTimeout(timeout)
         }
     }
 }
